ae090940bd9f7eaddf0e139099fe1b4188ada707
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-06-05 00:23:05 +0300

    Export certification PDF

--- src/main/java/org/race/loko/controllers/export/ExportController.java
+++ src/main/java/org/race/loko/controllers/export/ExportController.java
@@ -1,6 +1,8 @@
 package org.race.loko.controllers.export;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.race.loko.models.business.views.ClassementEquipe;
+import org.race.loko.repositories.business.views.ClassementEquipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Map;
 
 //@RestController
@@ -19,29 +22,36 @@ import java.util.Map;
 @Controller
 public class ExportController {
     ExportService exportService;
+    private final ClassementEquipeRepository classementEquipeRepository;
 
     @Autowired
-    public ExportController(ExportService exportService) {
+    public ExportController(ExportService exportService, ClassementEquipeRepository classementEquipeRepository) {
         this.exportService = exportService;
+        this.classementEquipeRepository = classementEquipeRepository;
     }
 
-    /*@GetMapping("/export")
+    @GetMapping("/export")
     @ResponseBody
     public void generatePdf(HttpServletResponse response) throws IOException {
+        List<ClassementEquipe> premiers = classementEquipeRepository.findBestEquipe();
+        ClassementEquipe prems = premiers.get(0);
+
         Map<String , Object> objects = Map.of(
-                "equipe" , "Equipe TEST",
-                "points", "69"
+                "course", prems.getCourse().getNom(),
+                "equipe" , prems.getEquipe().getNom(),
+                "rang", prems.getRangEquipe()+"e",
+                "points", prems.getPoints()
         );
 
         response.setContentType("application/pdf");
         response.setHeader("Content-Disposition", "attachment; filename=certificat.pdf");
 
         OutputStream writer = response.getOutputStream();
-        exportService.exportPdf("templates/export/exportPDF" , objects , writer);
+        exportService.exportPdf("templates/export/export" , objects , writer);
         writer.close();
 
-    }*/
-
+    }
+/*
     @GetMapping("/export")
     public String generatePdf(Model model) throws IOException {
 
@@ -50,5 +60,5 @@ public class ExportController {
 
         return "export/exportPDF";
     }
-
+*/
 }
